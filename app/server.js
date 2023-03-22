4026ce571f40a578d57d1c5f8daa819f5ddb4deb
samuraidays <1samuraiboy@gmail.com>
2023-03-22 19:03:28 +0800

    add db connect

--- app/server.js
+++ app/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8100;
+const mongoose = require("mongoose");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
+require("dotenv").config();
+
+//データベース接続
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("データベース接続中");
+    }).catch((error) => {
+        console.log(error);
+    });
 
 // ミドルウェアの設定
 app.use("/api/users", userRoute);
@@ -12,4 +22,4 @@ app.use("/api/post", postRoute);
 
 app.listen(PORT, () => {
     console.log("サーバが起動しました")
-})
+});
