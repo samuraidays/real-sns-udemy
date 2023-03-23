42174201c67c24a5792c62ef7c8a9e35114d247f
samuraidays <1samuraiboy@gmail.com>
2023-03-23 14:24:42 +0800

    create user update api

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
+// ユーザ情報の更新
+router.put("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try {
+            const loginuser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("ユーザ情報が更新されました")
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    } else {
+        return res.status(403).json("あなたは自分のアカウントのときだけ更新できます");
+    }
+    
+})
 
+// ユーザ情報の削除
+
+// ユーザ情報の取得
 
 module.exports = router;
