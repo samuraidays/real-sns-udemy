d4a38d30c0812fc3b32f836513546a0ce398374e
samuraidays <1samuraiboy@gmail.com>
2023-03-28 15:36:10 +0800

    29 done

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,7 +1,64 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
+const { updateOne } = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("post route");
+// 投稿を作成する
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savePost = await newPost.save();
+    return res.status(200).json(savePost);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
+// 投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功しました");
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を編集できません");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// 投稿を更新する
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("投稿削除に成功しました");
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を削除できません");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// 特定の投稿を取得する
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// 特定の投稿にいいねを押す
+router.put("/:id", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+  }
+});
 module.exports = router;
