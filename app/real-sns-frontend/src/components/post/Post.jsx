d52b58d1c701c0becd9c466b51abf256284e8c64
samuraidays <1samuraiboy@gmail.com>
2023-04-13 14:34:31 +0800

    54 done

--- app/real-sns-frontend/src/components/post/Post.jsx
+++ app/real-sns-frontend/src/components/post/Post.jsx
@@ -7,20 +7,27 @@ export default function Post() {
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
-          <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
-          <span className="postUsername">Shine code</span>
-          <spna className="postDate">5分前</spna>
+          <div className="postTopLeft">
+            <img
+              src="./assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUsername">Shine code</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-        <div className="postRight">
-          <MoreVert />
-        </div>
+
         <div className="postCenter">
           <span className="postText">SNSを自作中</span>
           <img src="./assets/post/1.jpeg" alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="./assets/post/heart.png" alt="" className="likeIcon" />
+            <img src="./assets/heart.png" alt="" className="likeIcon" />
             <span className="postLikeCounter">5人がいいねを押しました</span>
           </div>
           <div className="postBottomRight">
